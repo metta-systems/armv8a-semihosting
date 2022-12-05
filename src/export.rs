a0226b5477e19c20819e6ca0b5e5c28bf25e54a0
Berkus Decker <berkus+github@metta.systems>
2022-12-05 15:22:50 +0200

    fix: Reenable interrupts on error return

--- src/export.rs
+++ src/export.rs
@@ -6,42 +6,50 @@ use crate::hio::{self, HStderr, HStdout};
 
 static mut HSTDOUT: Option<HStdout> = None;
 
-#[inline(always)]
-unsafe fn int_disable() {
-    asm!("msr daifset, #0b0011");
+struct IntDisable;
+
+impl IntDisable {
+    #[inline(always)]
+    pub fn enter() -> IntDisable {
+        unsafe { asm!("msr daifset, #0b0011"); }
+        Self
+    }
 }
 
-#[inline(always)]
-unsafe fn int_enable() {
-    asm!("msr daifclr, #0b0011");
+impl Drop for IntDisable {
+    #[inline(always)]
+    fn drop(&mut self) {
+        unsafe { asm!("msr daifclr, #0b0011"); }
+    }
 }
 
 pub fn hstdout_str(s: &str) -> Result<(), ()> {
     unsafe {
-        int_disable();
+        let ints = IntDisable::enter();
 
         if HSTDOUT.is_none() {
             HSTDOUT = Some(hio::hstdout()?);
         }
 
         let ret = HSTDOUT.as_mut().unwrap().write_str(s).map_err(drop);
-        
-        int_enable();
-        return ret;
+
+        drop(ints);
+        ret
     }
 }
 
 pub fn hstdout_fmt(args: fmt::Arguments) -> Result<(), ()> {
     unsafe {
-        int_disable();
+        let ints = IntDisable::enter();
+
         if HSTDOUT.is_none() {
             HSTDOUT = Some(hio::hstdout()?);
         }
 
         let ret = HSTDOUT.as_mut().unwrap().write_fmt(args).map_err(drop);
 
-        int_enable();
-        return ret;
+        drop(ints);
+        ret
     }
 }
 
@@ -49,28 +57,30 @@ static mut HSTDERR: Option<HStderr> = None;
 
 pub fn hstderr_str(s: &str) -> Result<(), ()> {
     unsafe {
-        int_disable();
+        let ints = IntDisable::enter();
+
         if HSTDERR.is_none() {
             HSTDERR = Some(hio::hstderr()?);
         }
 
         let ret = HSTDERR.as_mut().unwrap().write_str(s).map_err(drop);
 
-        int_enable();
-        return ret;
+        drop(ints);
+        ret
     }
 }
 
 pub fn hstderr_fmt(args: fmt::Arguments) -> Result<(), ()> {
     unsafe {
-        int_disable();
+        let ints = IntDisable::enter();
+
         if HSTDERR.is_none() {
             HSTDERR = Some(hio::hstderr()?);
         }
 
         let ret = HSTDERR.as_mut().unwrap().write_fmt(args).map_err(drop);
 
-        int_enable();
-        return ret;
+        drop(ints);
+        ret
     }
 }
